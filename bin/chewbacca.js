d35dc92d747d5b1b06469d276cdb6645047c8549
Andreas Lind <andreas@one.com>
2015-09-14 21:58:21 +0200

    Avoid ending up in detached head mode after the benchmarks have run if a branch was checked out.

--- bin/chewbacca.js
+++ bin/chewbacca.js
@@ -54,89 +54,95 @@ exec('git diff-index --quiet HEAD', function (err, stdout, stderr) {
         }
     }
 
-    exec('git rev-parse HEAD', function (err, stdout, stderr) {
+    exec('git rev-parse --abbrev-ref HEAD', function (err, stdout, stderr) {
         if (err) {
             throw err;
         }
-        var originalSha = stdout.toString('utf-8').replace(/\n/, '');
-        async.eachLimit(refs, 1, function (ref, cb) {
-            if (ref === 'working dir') {
-                proceedToRunBenchmark();
-            } else {
-                if (dirtyWorkingTree) {
-                    exec('git stash', passError(cb, function () {
-                        exec('git checkout ' + ref.replace(/^HEAD/, originalSha), true, passError(cb, proceedToRunBenchmark));
-                    }));
-                } else {
-                    exec('git checkout ' + ref.replace(/^HEAD/, originalSha), true, passError(cb, proceedToRunBenchmark));
-                }
-            }
-            function proceedToRunBenchmark() {
-                exec('./node_modules/.bin/mocha ' +
-                     '--no-timeouts ' +
-                     '--ui chewbacca/mocha-benchmark-ui ' +
-                     '--reporter chewbacca/mocha-benchmark-reporter ' +
-                     mochaArgs, passError(cb, function (stdout, stderr) {
-                         var result = JSON.parse(stdout.toString('utf-8'));
-                         result.ref = ref;
-                         results.push(result);
-                         cb();
-                     }));
-            }
-        }, function (err) {
-            if (err) {
-                throw err;
-            }
-            var numValidResults = 0;
-            var sumRatios = 0;
-            console.log(results.map(function (result) {
-                return result.ref;
-            }).join(' vs. '));
-            results[0].passes.forEach(function (result, i) {
-                var otherResult = results[1].passes[i];
-                if (otherResult.fullTitle === result.fullTitle) {
-                    var difference = result.metadata.operationsPrSecond - otherResult.metadata.operationsPrSecond;
-                    var ratio = difference / otherResult.metadata.operationsPrSecond;
-                    numValidResults += 1;
-                    sumRatios += ratio;
-                    console.log(result.fullTitle,
-                                Math.round(result.metadata.operationsPrSecond),
-                                '/',
-                                Math.round(otherResult.metadata.operationsPrSecond),
-                                (100 * Math.abs(ratio)).toFixed(2) + '%',
-                                ratio < 0 ? 'slower' : 'faster'
-                               );
-                }
-            });
-            var avg = sumRatios / numValidResults;
-            console.log(results[0].ref,
-                        'is',
-                        (100 * Math.abs(avg)).toFixed(2) + '%',
-                        avg < 0 ? 'slower' : 'faster',
-                        'than',
-                        results[1].ref,
-                        'on average');
-            exec('git checkout ' + originalSha, true, function (err) {
-                function onFinish() {
-                    if (typeof argv.threshold === 'number' && argv.threshold <= -avg * 100) {
-                        console.log('Performance regression higher than threshold');
-                        process.exit(1);
-                    }
-                }
-                if (err) {
-                    throw err;
-                }
-                if (dirtyWorkingTree) {
-                    exec('git stash pop', function (err) {
-                        if (err) {
-                            throw err;
-                        }
-                        onFinish();
-                    });
-                } else {
-                    onFinish();
-                }
-            });
-        });
-    });
+        var originalRef = stdout.toString('utf-8').replace(/\n/, '');
+      exec('git rev-parse HEAD', function (err, stdout, stderr) {
+          if (err) {
+              throw err;
+          }
+          var originalSha = stdout.toString('utf-8').replace(/\n/, '');
+          async.eachLimit(refs, 1, function (ref, cb) {
+              if (ref === 'working dir') {
+                  proceedToRunBenchmark();
+              } else {
+                  if (dirtyWorkingTree) {
+                      exec('git stash', passError(cb, function () {
+                          exec('git checkout ' + ref.replace(/^HEAD/, originalSha), true, passError(cb, proceedToRunBenchmark));
+                      }));
+                  } else {
+                      exec('git checkout ' + ref.replace(/^HEAD/, originalSha), true, passError(cb, proceedToRunBenchmark));
+                  }
+              }
+              function proceedToRunBenchmark() {
+                  exec('./node_modules/.bin/mocha ' +
+                       '--no-timeouts ' +
+                       '--ui chewbacca/mocha-benchmark-ui ' +
+                       '--reporter chewbacca/mocha-benchmark-reporter ' +
+                       mochaArgs, passError(cb, function (stdout, stderr) {
+                           var result = JSON.parse(stdout.toString('utf-8'));
+                           result.ref = ref;
+                           results.push(result);
+                           cb();
+                       }));
+              }
+          }, function (err) {
+              if (err) {
+                  throw err;
+              }
+              var numValidResults = 0;
+              var sumRatios = 0;
+              console.log(results.map(function (result) {
+                  return result.ref;
+              }).join(' vs. '));
+              results[0].passes.forEach(function (result, i) {
+                  var otherResult = results[1].passes[i];
+                  if (otherResult.fullTitle === result.fullTitle) {
+                      var difference = result.metadata.operationsPrSecond - otherResult.metadata.operationsPrSecond;
+                      var ratio = difference / otherResult.metadata.operationsPrSecond;
+                      numValidResults += 1;
+                      sumRatios += ratio;
+                      console.log(result.fullTitle,
+                                  Math.round(result.metadata.operationsPrSecond),
+                                  '/',
+                                  Math.round(otherResult.metadata.operationsPrSecond),
+                                  (100 * Math.abs(ratio)).toFixed(2) + '%',
+                                  ratio < 0 ? 'slower' : 'faster'
+                                 );
+                  }
+              });
+              var avg = sumRatios / numValidResults;
+              console.log(results[0].ref,
+                          'is',
+                          (100 * Math.abs(avg)).toFixed(2) + '%',
+                          avg < 0 ? 'slower' : 'faster',
+                          'than',
+                          results[1].ref,
+                          'on average');
+              exec('git checkout ' + (originalRef === 'HEAD' ? originalSha : originalRef), true, function (err) {
+                  function onFinish() {
+                      if (typeof argv.threshold === 'number' && argv.threshold <= -avg * 100) {
+                          console.log('Performance regression higher than threshold');
+                          process.exit(1);
+                      }
+                  }
+                  if (err) {
+                      throw err;
+                  }
+                  if (dirtyWorkingTree) {
+                      exec('git stash pop', function (err) {
+                          if (err) {
+                              throw err;
+                          }
+                          onFinish();
+                      });
+                  } else {
+                      onFinish();
+                  }
+              });
+          });
+      });
+  });
 });
